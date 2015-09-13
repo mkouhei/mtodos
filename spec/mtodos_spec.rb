35db8b7c2a64a23512d7bdb667354fa7e11bc788
Kouhei Maeda <mkouhei@palmtb.net>
2015-09-14 05:35:58 +0900

    Fixes stub_request with query parameters.
    Signed-off-by: Kouhei Maeda <mkouhei@palmtb.net>

--- spec/mtodos_spec.rb
+++ spec/mtodos_spec.rb
@@ -4,20 +4,30 @@ require 'mtodos'
 
 include WebMock::API
 
+UDD = 'http://udd.debian.org/dmd/?email1=dummy%40example.org&format=json#todo'
+
 
 describe Mtodos do
   data = File.read('spec/data/udd.debian.org.json')
-  stub_request(:any, 'udd.debian.org').to_return(
-    :body => data,
-    :status => 200,
-    :headers => {'Content-Length' => 7632}
-  )
+  stub_request(:any, 'udd.debian.org/dmd/')
+    .with(:query => {:email1 => 'dummy@example.org',
+                     :format => 'json'})
+    .to_return(:body => data,
+               :status => 200,
+               :headers => {'Content-Length' => 7632})
+
+  after do
+    if File.exist?('mtodos.cache')
+      File.unlink('mtodos.cache')
+    end
+  end
+  
   it 'has a version number' do
     expect(Mtodos::VERSION).not_to be nil
   end
 
   it 'initialize Client with cache file' do
-    Mtodos::Client.new('https://udd.debian.org/dmd/?email1=dummy%40example.org&format=json#todo')
+    cli = Mtodos::Client.new(UDD)
     expect(File.exist?('mtodos.cache')).to eq(true)
   end
 end
